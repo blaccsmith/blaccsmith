cd82537a527905dc8cc1244ff5db617a5484673b
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-15 16:50:41 -0500

    chore: renamed variables

--- constants.ts
+++ constants.ts
@@ -6,8 +6,8 @@ export const CONSTANTS = Object.freeze({
     DISCORD_TOKEN: process.env.DISCORD_TOKEN as string,
     CLIENT_ID: process.env.CLIENT_ID as string,
     GUILD_ID: process.env.GUILD_ID as string,
-    RULES_CHANNEL_ID: process.env.RULES_CHANNEL_ID as string,
-    RULES_MESSAGE_ID: process.env.RULES_MESSAGE_ID as string,
+    COMMUNITY_GUIDELINES_CHANNEL_ID: process.env.COMMUNITY_GUIDELINES_CHANNEL_ID as string,
+    COMMUNITY_GUIDELINES_MESSAGE_ID: process.env.COMMUNITY_GUIDELINES_MESSAGE_ID as string,
     BOT_INTENTS: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
@@ -15,4 +15,6 @@ export const CONSTANTS = Object.freeze({
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
     ],
     BOT_PARTIALS: ['MESSAGE', 'CHANNEL', 'REACTION'] as PartialTypes[],
+    SPECTATOR_ROLE_ID: process.env.SPECTATOR_ROLE_ID as string,
+    MEMBER_ROLE_ID: process.env.MEMBER_ROLE_ID as string,
 });
