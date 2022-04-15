a1a8ab78232e341f62ccd2c32bc73b1a22ebf189
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-15 11:05:41 -0500

    refactor: added bot intents to constants

--- constants.ts
+++ constants.ts
@@ -1,3 +1,4 @@
+import { Intents } from 'discord.js';
 import { config } from 'dotenv';
 config();
 
@@ -5,4 +6,5 @@ export const CONSTANTS = Object.freeze({
     DISCORD_TOKEN: process.env.DISCORD_TOKEN as string,
     CLIENT_ID: process.env.CLIENT_ID as string,
     GUILD_ID: process.env.GUILD_ID as string,
+    BOT_INTENTS: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES],
 });
