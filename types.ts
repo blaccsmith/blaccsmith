686da989e18ae87c4cee0197358b5d3ca3bbc679
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-15 10:56:44 -0500

    chore: extracted client type

--- types.ts
+++ types.ts
@@ -1,7 +1,9 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, CacheType } from 'discord.js';
+import { CommandInteraction, CacheType, Client, Collection } from 'discord.js';
 
 export interface CommandType {
     data: SlashCommandBuilder;
     execute: (interaction: CommandInteraction<CacheType>) => Promise<void>;
 }
+
+export type ClientWithCommands = Client & { commands?: Collection<string, any> };
