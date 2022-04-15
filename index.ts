9c21d8d11f818649266f7ae2e412065d4622f3ae
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-14 22:13:38 -0500

    feat: respond to slash commands

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-import { Client, Intents } from 'discord.js';
-import { config } from 'dotenv';
-config();
+import { Client, Guild, Intents } from 'discord.js';
+import { CONSTANTS } from './constants';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -8,4 +7,22 @@ client.once('ready', () => {
     console.log('Ready player one!');
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        const guild = interaction.guild as Guild;
+        const message = `Server name: ${guild.name}\nTotal members: ${guild.memberCount}`;
+
+        await interaction.reply(message);
+    } else if (commandName === 'user') {
+        const message = `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`;
+        await interaction.reply(message);
+    }
+});
+
+client.login(CONSTANTS.DISCORD_TOKEN);
