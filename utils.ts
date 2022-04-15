fffa66ae590ff35a07a56b37648943672184fff9
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-15 10:57:01 -0500

    feat: added util to get event files

--- utils.ts
+++ utils.ts
@@ -1,3 +1,4 @@
 import fs from 'node:fs';
 
 export const getCommandFiles = () => fs.readdirSync('./dist/commands').filter(file => file.endsWith('.js'));
+export const getEventFiles = () => fs.readdirSync('./dist/events').filter(file => file.endsWith('.js'));
