e19394491c352cb4fb94804638cd1ae1fdf7ed3d
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-16 18:36:47 -0500

    chore: re-added commands script

---
+++ commands.ts
@@ -0,0 +1,21 @@
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import { CONSTANTS } from './constants';
+import { getCommandFiles } from './utils';
+
+const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = CONSTANTS;
+
+const commands = [];
+
+for (const file of getCommandFiles()) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN as string);
+
+rest.put(Routes.applicationGuildCommands(CLIENT_ID as string, GUILD_ID as string), {
+    body: commands,
+})
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
