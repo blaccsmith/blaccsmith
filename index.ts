7cea7fadcbed3b353420d9afdf74b445a43bb48d
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-16 18:37:31 -0500

    feat: listen to slash commands

--- index.ts
+++ index.ts
@@ -16,6 +16,24 @@ for (const file of getCommandFiles()) {
     client.commands.set(command.data.name, command);
 }
 
+// Listen for slash commands
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands?.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true,
+        });
+    }
+});
+
 // Register events that we'll listen to
 for (const file of getEventFiles()) {
     const event = require(`./events/${file}`);
