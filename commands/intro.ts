462f6dd06586bc36081861c7d94b04d1192677a3
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-16 18:39:15 -0500

    feat: get response to slash command

---
+++ commands/intro.ts
@@ -0,0 +1,38 @@
+import { CommandInteraction, CacheType } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+export const data = new SlashCommandBuilder()
+    .setName('intro')
+    .setDescription('✨ Tell us a bit about yourself!')
+    .addStringOption(option =>
+        option
+            .setName('status')
+            .setDescription(`How would you describe yourself?`)
+            .setRequired(true)
+            .addChoices([
+                ['🌱 Aspiring Developer', 'Aspiring'],
+                ['🪴 Junior Developer', 'Junior'],
+                ['🌿 Mid-level Developer', 'Mid-level'],
+                ['🌳 Senior Developer', 'Senior'],
+            ]),
+    )
+    .addStringOption(option =>
+        option.setName('github').setDescription('What is your GitHub username?'),
+    )
+    .addStringOption(option =>
+        option.setName('linkedin').setDescription('What is your LinkedinIn username?'),
+    )
+    .addStringOption(option =>
+        option.setName('twitter').setDescription('What is your Twitter handle?'),
+    );
+
+export async function execute(interaction: CommandInteraction<CacheType>) {
+    const status = interaction.options.get('status');
+    const github = interaction.options.getString('github');
+    const linkedin = interaction.options.getString('linkedin');
+    const twitter = interaction.options.getString('twitter');
+
+    console.log({ status, github, linkedin, twitter });
+
+    await interaction.reply({ content: 'Welcome to the server!', ephemeral: true });
+}
