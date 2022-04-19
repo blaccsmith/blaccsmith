e8f25b45d6fe9b62b069abbfb26fd89abd8f1216
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-18 19:47:40 -0500

    refactor: added example command to repo

--- commands/example.ts
+++ commands/example.ts
@@ -1,9 +1,7 @@
 import { CommandInteraction, CacheType } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
-export const data = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!');
+export const data = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!');
 
 export async function execute(interaction: CommandInteraction<CacheType>) {
     await interaction.reply('Pong!');
