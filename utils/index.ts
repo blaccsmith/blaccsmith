5698a6c70ae8a9594bee077733b39979be2fec00
Darryl Brooks <darrylbrooks13@gmail.com>
2022-06-17 20:33:44 -0500

    chore: moving function

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,25 @@
+import { ColorResolvable, EmbedAuthorData, MessageEmbed } from 'discord.js';
 import fs from 'node:fs';
 
-export const getCommandFiles = () => fs.readdirSync('./dist/commands').filter(file => file.endsWith('.js'));
-export const getEventFiles = () => fs.readdirSync('./dist/events').filter(file => file.endsWith('.js'));
+export const getCommandFiles = () =>
+    fs.readdirSync('./dist/commands').filter(file => file.endsWith('.js'));
+export const getEventFiles = () =>
+    fs.readdirSync('./dist/events').filter(file => file.endsWith('.js'));
+export const embedMessage = ({
+    title,
+    description,
+    color,
+    author,
+}: {
+    title: string;
+    description: string;
+    color: ColorResolvable;
+    author: EmbedAuthorData;
+}) => {
+    return new MessageEmbed()
+        .setColor(color)
+        .setTitle(title)
+        .setAuthor(author)
+        .setDescription(description)
+        .setTimestamp();
+};
