e43f7dd37b2f26008e02660a905934af5b2368ee
Darryl Brooks <darrylbrooks13@gmail.com>
2022-06-18 13:18:10 -0500

    feat: adding links to embeded message

--- utils/index.ts
+++ utils/index.ts
@@ -1,22 +1,51 @@
-import { ColorResolvable, EmbedAuthorData, MessageEmbed } from 'discord.js';
+import { ColorResolvable, EmbedAuthorData, MessageEmbed, MessageEmbedThumbnail } from 'discord.js';
 import fs from 'node:fs';
 
 export const getCommandFiles = () =>
     fs.readdirSync('./dist/commands').filter(file => file.endsWith('.js'));
+
 export const getEventFiles = () =>
     fs.readdirSync('./dist/events').filter(file => file.endsWith('.js'));
+
+export const formatUrl = (url: string) => {
+    if (url.startsWith('https://www.')) {
+        return url;
+    } else if (url.startsWith('www.')) {
+        return `https://${url}`;
+    }
+    return `https://www.${url}`;
+};
+
 export const embedMessage = ({
     title,
     description,
     color,
     author,
+    thumbnail,
+    links,
 }: {
     title: string;
     description: string;
     color: ColorResolvable;
     author: EmbedAuthorData;
+    thumbnail?: string;
+    links?: { name: string; value: string; inline: boolean }[];
 }) => {
-    return new MessageEmbed()
+    let message = new MessageEmbed();
+
+    if (thumbnail) {
+        message
+            .setColor(color)
+            .setTitle(title)
+            .setAuthor(author)
+            .setDescription(description)
+            .setTimestamp()
+            .setThumbnail(thumbnail);
+        links?.map(link => message.addField(link.name, link.value, link.inline));
+        return message;
+    }
+
+    return message
         .setColor(color)
         .setTitle(title)
         .setAuthor(author)
