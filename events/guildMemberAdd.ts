ac8b2c1c434a13a0b20d4182f5be53b3676ea4bb
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-07-07 19:38:11 -0500

    refactor: updated new member dm

--- events/guildMemberAdd.ts
+++ events/guildMemberAdd.ts
@@ -2,7 +2,15 @@ import { GuildMember } from 'discord.js';
 
 export const name = 'guildMemberAdd';
 
+const welcomeMessage = `
+**Welcome to BLACC 👋🏾 We're a community of like-minded developers from around the world 🌍 focused on educating, collaborating with, and uplifting each other! To keep BLACC a safe and open environment for all, you need to do two things:**
+
+- In \`#community-guidelines\`, react to our community guidelines with a ✅
+- In \`#welcome\`, introduce yourself using the \`\/intro\` command ✨
+
+> **We hope this space is inviting and impactful to you! Some pro tips – tell us about a project your working on in \`#showcase\` is perfect for that.**
+`;
+
 export const execute = async (member: GuildMember) => {
-    console.log({ member });
-    const role = member.user.send('Welcome to the server!');
+    await member.user.send(welcomeMessage);
 };
