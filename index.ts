73eb5a9ebb68734b02958d835d1f19a8bd137d40
Darryl Brooks <darrylbrooks13@gmail.com>
2022-07-08 00:25:11 -0500

    feat: init good new friday

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Client, Collection } from 'discord.js';
+import { Client, Collection, GuildScheduledEventCreateOptions } from 'discord.js';
 import { CONSTANTS } from './constants';
 import { ClientWithCommands } from './types';
 import { getCommandFiles, getEventFiles } from './utils/get-sys-files';
@@ -45,3 +45,34 @@ for (const file of getEventFiles()) {
 }
 
 client.login(CONSTANTS.DISCORD_TOKEN);
+
+const cron = require('node-cron');
+
+cron.schedule(
+    '* 0 9 * * FRI',
+    () => {
+        const generalId = '988246104892145704';
+        const options: GuildScheduledEventCreateOptions = {
+            name: 'Good News Friday',
+            channel: generalId,
+            privacyLevel: 'GUILD_ONLY',
+            description: 'Share your good news!',
+            entityType: 'EXTERNAL',
+            scheduledStartTime: new Date(Date.now() + 3600 * 1000 * 24),
+            scheduledEndTime: new Date(Date.now() + 3600 * 1000 * 24 * 2),
+            entityMetadata: {
+                location: '#general channel',
+            },
+        };
+
+        const guild = client.guilds.cache.get(CONSTANTS.GUILD_ID);
+
+        if (!guild) return;
+
+        guild.scheduledEvents.create(options);
+    },
+    {
+        scheduled: true,
+        timezone: 'America/Chicago',
+    },
+);
