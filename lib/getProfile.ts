5307b4444dead1c27e0a3f59a8ebbbe76507dafc
Darryl Brooks <darrylbrooks13@gmail.com>
2022-07-13 20:30:44 -0500

    feat: method retrieves user from db

--- lib/getProfile.ts
+++ lib/getProfile.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
 interface GetProfileArgs {
-    userId: string;
+    discordId: string;
 }
 
 export const getProfile = async (args: GetProfileArgs) => {
     const prisma = new PrismaClient();
 
     return prisma.profile.findFirst({
-        where: { userId: args.userId },
+        where: { discordId: args.discordId },
     });
 };
