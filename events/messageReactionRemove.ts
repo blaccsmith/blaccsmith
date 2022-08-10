a1cca6d40a8539b24f3356d4b083369689eee5d7
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-08-10 17:58:16 -0500

    refactor: reverted logic

--- events/messageReactionRemove.ts
+++ events/messageReactionRemove.ts
@@ -7,22 +7,15 @@ export const name = 'messageReactionRemove';
 
 export const execute = async (reaction: MessageReaction, user: User) => {
     if (reaction.message.id !== CONSTANTS.COMMUNITY_GUIDELINES_MESSAGE_ID) return;
-    if (reaction.emoji.name !== '✅') {
-        try {
-            await reaction.remove();
-            return;
-        } catch (error) {
-            console.error('Failed to remove reactions.');
-        }
-    }
 
     const guild = await client.guilds.fetch(CONSTANTS.GUILD_ID);
     const member = await guild.members.fetch(user.id);
 
-    if (member.roles.cache.has(CONSTANTS.MEMBER_ROLE_ID)) {
-        await member.roles.remove(CONSTANTS.MEMBER_ROLE_ID);
-    } else {
+    if (member.roles.cache.has(CONSTANTS.SPECTATOR_ROLE_ID)) {
         await member.roles.remove(CONSTANTS.SPECTATOR_ROLE_ID);
+    } else if (member.roles.cache.has(CONSTANTS.MEMBER_ROLE_ID)) {
+        await member.roles.remove(CONSTANTS.MEMBER_ROLE_ID);
+        await member.roles.add(CONSTANTS.SPECTATOR_ROLE_ID);
     }
 
     await logger({
