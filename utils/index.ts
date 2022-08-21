bd5e214c59883ae04217bf390715f72ce6316d18
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-21 14:54:21 -0400

    feat: notifies searched user for profile creation

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CacheType, CommandInteraction, GuildMember } from 'discord.js';
+
 export const socials = {
     github: 'https://github.com',
     linkedin: 'https://linkedin.com/in',
@@ -11,3 +13,18 @@ export const formatSocial = (name: keyof typeof socials, value: string) => {
 
 export const getLinkName = (link: string) =>
     link?.includes('github') ? 'github' : link?.includes('linkedin') ? 'linkedin' : 'twitter';
+
+export const messageReply = async (
+    interaction: CommandInteraction<CacheType>,
+    message: string,
+    searchedMember: GuildMember,
+) => {
+    const username = interaction.options.getString('username');
+    return await Promise.all([
+        interaction.reply({
+            content: `${username} does not have their profile configured`,
+            ephemeral: true,
+        }),
+        searchedMember.send(message),
+    ]);
+};
