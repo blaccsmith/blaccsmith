21b3757ffb522189a70ff5656a9447898732ee39
BLACC <blaccsmith.xyz@gmail.com>
2022-09-26 20:29:23 -0500

    Update src/automations/create-gnf-thread.ts
    Co-authored-by: Darryl Brooks <24500251+DarrylBrooks97@users.noreply.github.com>

--- src/automations/create-gnf-thread.ts
+++ src/automations/create-gnf-thread.ts
@@ -7,7 +7,6 @@ export default async function run() {
     const date = formatISO(new Date(), { representation: 'date' });
     const guild = client.guilds.cache.get(CONSTANTS.GUILD_ID);
     const channel = guild?.channels.cache.get(CONSTANTS.GENERAL_CHANNEL_ID) as TextChannel;
-    console.log('works !');
 
     const createdMessage = await channel.send({
         content: `It's Good News Friday and we want to celebrate your wins this week <@&${CONSTANTS.MEMBER_ROLE_ID}> 🥳 Comment in this thread with your small wins, something exciting that happened, etc. this week! Can be related to your job, a side project, or life in general - we wanna know about it and celebrate you!`,
