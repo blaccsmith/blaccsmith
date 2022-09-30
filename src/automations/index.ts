1eba890a7e0b620b96071524c697388fa1d211cc
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-09-30 15:47:45 -0500

    refactor: patch refactor for automations

--- src/automations/index.ts
+++ src/automations/index.ts
@@ -2,32 +2,26 @@ import { schedule } from 'node-cron';
 import { GuildScheduledEventCreateOptions } from 'discord.js';
 import { client } from '..';
 import { CONSTANTS } from '../constants';
-import automations from './index.json';
+import { events, jobs } from './index.json';
 
 const scheduleOptions = { timezone: 'America/Chicago' };
 
 export default function run() {
-    automations.forEach(({ type, ...details }) => {
-        if (type === 'job') {
-            const callback = require(`./${details.callback}`);
-            schedule(details.expression, callback.default, scheduleOptions);
-        } else {
-            const guild = client.guilds.cache.get(CONSTANTS.GUILD_ID);
-            const channel = guild?.channels.cache.find(
-                ({ name }) => name === automations[0].channel,
-            )?.id;
+    jobs.forEach(job => {
+        const callback = require(`./${job.callback}`);
+        schedule(job.expression, callback.default, scheduleOptions);
+    });
 
-            const options = {
-                ...details,
-                ...CONSTANTS.DEFAULT_EVENT_OPTIONS,
-                channel,
-            } as GuildScheduledEventCreateOptions;
+    events.forEach(event => {
+        const guild = client.guilds.cache.get(CONSTANTS.GUILD_ID);
+        const channel = guild?.channels.cache.find(({ name }) => name === event.channel)?.id;
 
-            schedule(
-                details.expression,
-                () => guild?.scheduledEvents.create(options),
-                scheduleOptions,
-            );
-        }
+        const options = {
+            ...event,
+            ...CONSTANTS.DEFAULT_EVENT_OPTIONS,
+            channel,
+        } as GuildScheduledEventCreateOptions;
+
+        schedule(event.expression, () => guild?.scheduledEvents.create(options), scheduleOptions);
     });
 }
