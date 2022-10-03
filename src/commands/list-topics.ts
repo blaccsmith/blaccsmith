b75ac42352ad45592c64f970085981e97f8f984a
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-10-03 12:55:47 -0500

    refactor: including id with topic

--- src/commands/list-topics.ts
+++ src/commands/list-topics.ts
@@ -1,9 +1,8 @@
 import { CommandInteraction, CacheType, GuildMember } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CONSTANTS } from '../constants';
-import { addTopic } from '../lib/addTopic';
 import logger from '../utils/logger';
 import { listTopics } from '../lib/listTopics';
+import { formatTopics } from '../lib/formatTopics';
 
 export const data = new SlashCommandBuilder()
     .setName('list-topics')
@@ -13,18 +12,12 @@ export async function execute(interaction: CommandInteraction<CacheType>) {
     const member = interaction.member as GuildMember;
     const topics = await listTopics();
 
-    const notUsed = topics
-        .filter(item => !item.used)
-        .map(item => item.topic)
-        .join('\n');
-    const used = topics
-        .filter(item => item.used)
-        .map(item => item.topic)
-        .join('\n');
+    const notUsed = topics.filter(item => !item.used);
+    const used = topics.filter(item => item.used);
 
     await Promise.all([
         interaction.reply({
-            content: `Not used:\n${notUsed}\n\nUsed:\n${used}`,
+            content: `Not used:\n${formatTopics(notUsed)}\n\nUsed:\n${formatTopics(used)}`,
             ephemeral: true,
         }),
         logger({
